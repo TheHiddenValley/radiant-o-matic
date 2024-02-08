22d05c5a712e24da2627dc286da1d48465c64870
The Hidden Valley <108282277+TheHiddenValley@users.noreply.github.com>
2024-02-09 02:34:45 +0800

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import alpacaImage from "../public/c25717a74e5d99f3cc847317b9d0e4b6.png"; // Assuming the image is located in the same folder as your component
+import alpacaImage from "../c25717a74e5d99f3cc847317b9d0e4b6.png"; // Assuming the image is located in the same folder as your component
 
 const Header = () => {
     return (
