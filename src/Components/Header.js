94995972cbed1da33a4c628e45dc980ced7b6b7d
The Hidden Valley <108282277+TheHiddenValley@users.noreply.github.com>
2024-02-09 02:37:53 +0800

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,9 @@
 import React from "react";
-import alpacaImage from "../c25717a74e5d99f3cc847317b9d0e4b6.png"; // Assuming the image is located in the same folder as your component
 
 const Header = () => {
     return (
-        <div>
-            <img src={alpacaImage} alt="Alpaca" className="alpaca-image" />
-        </div>
+        <h1 className="header">RADIANT O MATIC</h1>
     )
 }
 
-export default Header;
+export default Header 
